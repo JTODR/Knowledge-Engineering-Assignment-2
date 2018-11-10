f2da84800a6bd68d1162db7c829c132f920135e5
Joseph <josephodr@hotmail.com>
2018-11-10 15:51:49 +0000

    Changed query results to hashmap of lists

--- src/main/java/com/knowledgeengineering/remote/RemoteJenaMain.java
+++ src/main/java/com/knowledgeengineering/remote/RemoteJenaMain.java
@@ -6,7 +6,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 
 // Jena
 import org.apache.jena.query.QueryExecution;
@@ -24,24 +26,32 @@ public class RemoteJenaMain {
 		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.OFF);
 
 		// Get the selected query ID from the front end
-		int queryId = getQueryIdFromFrontend();
-    String params[] = getParamsFromFrontend();
+		//int queryId = getQueryIdFromFrontend();
+		//String params[] = getParamsFromFrontend();
 		// Read in the query string for the received query ID
-		String query= getQuery(queryId, params);
+		String query= getQuery(1, null);
 
 		System.out.println(query);
 
 		// Send the query to Parliament and get the results
 		System.out.println("Sending query to Parliament...");
-		String[] queryResults = issueSelectQuery(query);
+		HashMap<String, ArrayList<String>> queryResults = issueSelectQuery(query);
 		if(queryResults == null) {
 			System.err.println("A query result is not a resource or literal, exiting...");
 			System.exit(2);
 		}
+		
+		for(HashMap.Entry<String, ArrayList<String>> pair : queryResults.entrySet()) {
+			String columnName = pair.getKey();
+			System.out.println("Column: " + columnName);
+			for(String result : pair.getValue()) {
+				System.out.println(result);
+			}
+		}
 
 		// Send the results back to the front end
 		System.out.println("Sending query results to front end...");
-		sendQueryResults(queryResults);
+		//sendQueryResults(queryResults);
 
 	}
 
@@ -58,13 +68,24 @@ public class RemoteJenaMain {
 		// TODO: send result to front end
 	}
 
-	private static String[] issueSelectQuery(String query) {
+	private static HashMap<String, ArrayList<String>> issueSelectQuery(String query) {
 		QueryExecution exec = QueryExecutionFactory.sparqlService(SERVER_URL, query);
 		ResultSet rs = exec.execSelect();
 
-		ArrayList<String> queryResults = new ArrayList<String>();
-		String result = "";
-
+		// HashMap of {column name : list of results for that column}
+		HashMap<String, ArrayList<String>> resultHmap = new LinkedHashMap<String, ArrayList<String>>();
+		
+		// Get the column names from the returned Parliament results
+		// Add the column names to the hash map and initialize a new ArrayList for that column name
+		Iterator<String> columnNames = rs.next().varNames();
+		while(columnNames.hasNext()) {
+			String columnName = columnNames.next();
+			resultHmap.put(columnName, new ArrayList<String>());
+		}
+		
+		// currentResult will be used to add to the ArrayList at each column name
+		ArrayList<String> currentResult = new ArrayList<String>();
+		
 		while(rs.hasNext()) {
 			// Get current query result set
 		    QuerySolution qs = rs.next() ;
@@ -79,21 +100,28 @@ public class RemoteJenaMain {
 
 		    	RDFNode node = qs.get(column);
 		    	if(node.isLiteral()) {
-		    		result = result + qs.getLiteral(column).toString() + " | ";
+		    		// Get the ArrayList currently stored at the column name
+		    		currentResult = resultHmap.get(column);
+		    		
+		    		// Add our new value to the ArrayList
+		    		currentResult.add(qs.getLiteral(column).toString());
+		    		
+		    		// Store the update ArrayList at the column name
+		    		resultHmap.put(column, currentResult);
+		    		
 		    	} else if(node.isResource()){
-		    		result = result + qs.getResource(column).toString() + " | ";
+		    		// Same as for isLiteral() above except the result this time is a Resource
+		    		currentResult = resultHmap.get(column);
+		    		currentResult.add(qs.getResource(column).toString());
+		    		resultHmap.put(column, currentResult);
 		    	} else {
 		    		return null;
 		    	}
 	    	}
-
-		    System.out.println(result);
-		    queryResults.add(result);
-		    result = "";
 		}
 		exec.close() ;
 
-		return queryResults.toArray(new String[0]);
+		return resultHmap;
 	}
 
 	private static String getQuery(int queryId, String params[]) {
@@ -112,10 +140,13 @@ public class RemoteJenaMain {
 		}
 		String ret = new String(fileBytes);
 
-		// do the parameter replacement
-		for(int i = 0; i < params.length; i++) {
-			ret = ret.replace("%PARAM_"+i+"%", params[i]);
+		if(params != null) {
+			// do the parameter replacement
+			for(int i = 0; i < params.length; i++) {
+				ret = ret.replace("%PARAM_"+i+"%", params[i]);
+			}	
 		}
+		
 		return  ret;
 	}
 
